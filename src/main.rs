9c586b17a5fb4718696bf25bd8eea6e00c4e069d
xinster <xinster@users.noreply.github.com>
2021-01-13 18:51:54 +0800

    project creation

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("This is the first homework of class 4. Use enum traffice light to build a trait. The trait has a function to return duration. Different light has different duration.");
+    
+}
