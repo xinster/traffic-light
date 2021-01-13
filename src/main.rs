b569a793ad405dc8e047cb96f7c9eea2ea4e82f8
xinster <xinster@users.noreply.github.com>
2021-01-13 19:33:52 +0800

    finish the homework

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
 fn main() {
-    println!("This is the first homework of class 4. Use enum traffice light to build a trait. The trait has a function to return duration. Different light has different duration.");
-    
+    println!("This is the first homework of class 4. Use enum traffice light to build a trait. The trait has a function to return duration. Different light has different duration.\n");
+    println!("Red traffic light durstion is {} seconds", LightColor::light_time(LightColor::Red));
+    println!("Green traffic light durstion is {} seconds", LightColor::light_time(LightColor::Green));
+    println!("Yellow traffic light durstion is {} seconds", LightColor::light_time(LightColor::Yellow));
+
+}
+
+pub enum LightColor{
+    Red,
+    Green,
+    Yellow
+}
+
+pub trait Lights {
+    fn light_time(&self)->LightColor;
+
+}
+
+impl LightColor{
+    pub fn light_time(light:LightColor)->u8{
+        match light{
+            LightColor::Red =>15,
+            LightColor::Green =>30,
+            LightColor::Yellow =>5
+        }
+    }
 }
